863edf1b48e68e9e45b07726fa002fa648ea5811
Rafal Adamczak <raad@fizyka.umk.pl>
2022-07-14 18:28:50 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uQlust_dist_Mac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
